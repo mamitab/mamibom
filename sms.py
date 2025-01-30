f39b9b6190646231982d872400ad9e980b91ea0c
Mamiozp <mamitablet21@gmail.com>
2025-01-30 04:54:42 +0300

    sms.py

--- sms.py
+++ sms.py
@@ -8,13 +8,63 @@ class SendSms():
     adet = 0
     
     def __init__(self, phone, mail):
-        self.phone = str(phone)
+       self.phone = str(phone)
         if len(mail) != 0:
             self.mail = mail
         else:
             self.mail = ''.join(choice(ascii_lowercase) for i in range(20))+"@gmail.com"
 
+        #erdeger.com.tr
+        def erdeger(self):
+        try:
+            url = requests.post("https://www.erdeger.com.tr/biz-sizi-arayalim", data={
+                "confirm": "true",
+                "date_of_birth": "1956-03-01",
+                "email": self.mail,
+                "email_allowed": "true",
+                "first_name": "Memati",
+                "gender": "male",
+                "last_name": "Bas",
+                "password": "31ABC..abc31",
+                "phone": f"0{self.phone}"
+            }, timeout=6)
+            if wmf.status_code == 202:
+                print(f"{Fore.LIGHTGREEN_EX}[+] {Style.RESET_ALL}Başarılı! {self.phone} --> wmf.com.tr")
+                self.adet += 1   
+            else:
+                raise
+        except:
+            print(f"{Fore.LIGHTRED_EX}[-] {Style.RESET_ALL}Başarısız! {self.phone} --> wmf.com.tr")
+    
 
+
+      
+      
+        
+        #elexushotel.com.tr
+        def elexushotel(self):
+        try:
+            url = requests.post("https://www.elexushotel.com.tr/tr/biz-sizi-arayalim", data={
+                "confirm": "true",
+                "date_of_birth": "1956-03-01",
+                "email": self.mail,
+                "email_allowed": "true",
+                "first_name": "Memati",
+                "gender": "male",
+                "last_name": "Bas",
+                "password": "31ABC..abc31",
+                "phone": f"0{self.phone}"
+            }, timeout=6)
+            if wmf.status_code == 202:
+                print(f"{Fore.LIGHTGREEN_EX}[+] {Style.RESET_ALL}Başarılı! {self.phone} --> wmf.com.tr")
+                self.adet += 1   
+            else:
+                raise
+        except:
+            print(f"{Fore.LIGHTRED_EX}[-] {Style.RESET_ALL}Başarısız! {self.phone} --> wmf.com.tr")
+    
+  
+   
     #kahvedunyasi.com
     def KahveDunyasi(self):    
         try:    
