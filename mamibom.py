9374b026141fea14193706013418fbd2a17aa4f9
Mamiozp <mamitablet21@gmail.com>
2025-02-02 20:45:53 +0300

    Update mamibom.py

--- mamibom.py
+++ mamibom.py
@@ -5,10 +5,18 @@ import time
 try:
     with ThreadPoolExecutor() as executor:
         futures = [
+            executor.submit(send_sms.Trendyol),
+            executor.submit(send_sms.Getir),
+            executor.submit(send_sms.Dsmart),
+            executor.submit(send_sms.Uber),
+            executor.submit(send_sms.Kimgbister),
+            executor.submit(send_sms.asiselektronik),
+            executor.submit(send_sms.Evidea),
             executor.submit(send_sms.Dominos),
             executor.submit(send_sms.Englishhome),
             executor.submit(send_sms.Evidea),
-            executor.submit(send_sms.File),
+            executor.submit(send_sms.File)
+        ]
         wait(futures)
 except KeyboardInterrupt:
     os.system("cls||clear")
